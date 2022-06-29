c08bc838e8e86bd5dad8a022f72c8b83c52cff55
vaibhav <vaibhavsachdeva2000@gmail.com>
2022-06-29 20:19:35 +0530

    ok

--- my/src/pages/Newmeetup.js
+++ my/src/pages/Newmeetup.js
@@ -1,11 +1,29 @@
 import Newmeetupform from '../components/layout/meetups/Newmeetupform'
-
+import { useNavigate } from 'react-router-dom';
 
 function Newmeetups()
 {
+    const navigate = useNavigate();
+
+   function getmeetupdata(meetupdata)
+   {
+      fetch('https://reactd1-default-rtdb.firebaseio.com/meetups.json',
+      {
+        method: 'POST' , 
+        body: JSON.stringify(meetupdata),
+        headers : {
+            'Content-Type' : 'application/json'
+        }
+      }
+      ).then(()=>
+      {
+        navigate('/');
+      });
+   }
+
     return <section>
          <h1>Add New Meetup</h1>
-         <Newmeetupform/>
+         <Newmeetupform onaddmeetup = {getmeetupdata}/>
     </section>;
 };
 
