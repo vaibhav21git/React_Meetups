eaf168f88397f44b3d7ff58fea79eeb8a8141854
vaibhav <vaibhavsachdeva2000@gmail.com>
2022-06-29 20:19:28 +0530

    ok

--- my/src/pages/Allmeetups.js
+++ my/src/pages/Allmeetups.js
@@ -1,30 +1,52 @@
 import Meetuplist from "../components/layout/meetups/Meetuplist";
+import { useState, useEffect } from "react";
 
-const DUMMY_DATA = [
-    {
-      id: 'm1',
-      title: 'This is a first meetup',
-      image:'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-      address: 'Meetupstreet 5, 12345 Meetup City',
-      description:
-        'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-    },
-    {
-      id: 'm2',
-      title: 'This is a second meetup',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-      address: 'Meetupstreet 5, 12345 Meetup City',
-      description:
-        'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-    },
-  ];
+function Allmeetups() {
+  const [isloading, setisloading] = useState(true);
+  const [loadedmeetups, setloadedmeetups] = useState([]);
 
-function Allmeetups(){
+  useEffect(() => {
 
-    return <section>
-         <h1>All Meetups</h1>
-         <Meetuplist meetups = {DUMMY_DATA}/>
-    </section>;
+       setisloading(true);
+
+    fetch('https://reactd1-default-rtdb.firebaseio.com/meetups.json')
+      .then((response) => { return response.json(); })
+      .then((data) => {
+
+        const meetups = [];
+
+        for(const key in data)
+        {
+           const meetup = {
+                  id  : key,
+                  ...data[key]
+           };
+           
+            // console.log(meetup.image);
+            meetups.push(meetup);
+        }
+        setisloading(false);
+        setloadedmeetups(meetups);
+      });
+  }, []);
+
+
+
+
+  if (isloading) {
+    return (<section>
+      <p>Loading......</p>
+    </section>
+    );
+  }
+
+  return <section>
+    <h1>All Meetups</h1>
+    <Meetuplist meetups={loadedmeetups} />
+  </section>;
 }
 
 export default Allmeetups;
+
+
+// with the use of useeffect the infinite loop is stopped 
